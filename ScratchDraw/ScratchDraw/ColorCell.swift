02ba5c50850dc49008a31fb52f87edd62e69b5f0
Whitney Hogg <whitneylaurenhogg@gmail.com>
2015-05-18 10:18:12 -0400

    fun with shapes

---
+++ ScratchDraw/ScratchDraw/ColorCell.swift
@@ -0,0 +1,24 @@
+//
+//  ColorCell.swift
+//  ScratchDraw
+//
+//  Created by Whitney Lauren on 5/15/15.
+//  Copyright (c) 2015 Whitney Lauren. All rights reserved.
+//
+
+import UIKit
+
+class ColorCell: UICollectionViewCell {
+    
+    
+
+    
+    
+        
+      
+            
+            
+    }
+    
+    
+
--- ScratchDraw/ScratchDraw/ViewController.swift
+++ ScratchDraw/ScratchDraw/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var fillColors: [UIColor] = [
+    UIColor.magentaColor(),
+    UIColor.cyanColor(),
+    UIColor.yellowColor(),
+    UIColor.blackColor()
+    
+]
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    @IBOutlet weak var viewBottomConstraint: NSLayoutConstraint!
+    
+    
+    
+    
+    @IBOutlet weak var fillColor: UICollectionView!
+    
     
     
     
@@ -26,10 +43,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       // view.addSubview(scratchPad)
-       // scratchPad.frame = view.frame
-       // scratchPad.backgroundColor = UIColor.whiteColor()
+        fillColor.dataSource = self
         
+        fillColor.delegate = self
+        
+        viewBottomConstraint.constant = -300
+        
+        
+        
+        // view.addSubview(scratchPad)
+        // scratchPad.frame = view.frame
+        // scratchPad.backgroundColor = UIColor.whiteColor()
+        
+        
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return fillColors.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("colorCell", forIndexPath: indexPath) as! ColorCell
+        
+        cell.backgroundColor = fillColors[indexPath.item]
+        
+        return cell
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        let cell = collectionView.cellForItemAtIndexPath(indexPath) as! ColorCell
+        
+        if let color = cell.backgroundColor {
+            
+            scratchPad.currentColor = color
+            
+        }
         
         
     }
@@ -39,11 +92,11 @@ class ViewController: UIViewController {
     
     @IBAction func changeColor(sender: UIButton) {
         
-        if let color = sender.backgroundColor {
+       if let color = sender.backgroundColor {
             
             scratchPad.currentColor = color
-        
-        
+            
+            
         }
         
         
@@ -52,13 +105,13 @@ class ViewController: UIViewController {
     
     
     
-
+    
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         
         if let touch = touches.first as? UITouch {
             
             let location = touch.locationInView(scratchPad)
-        
+            
             scratchPad.newScratchWithStartPoint(location)
             
             
@@ -74,14 +127,27 @@ class ViewController: UIViewController {
         if let touch = touches.first as? UITouch {
             
             let location = touch.locationInView(scratchPad)
-            //scratchPad.updateCurrentLineWithLastPoint(location)
-            scratchPad.addPointToCurrentScratch(location)
+            
+            scratchPad.updateCurrentScratchWithLastPoint(location)
+            
+            //scratchPad.addPointToCurrentScratch(location)
+            
+            
             
             
             
         }
-    
+       
+        
     }
+    
+    
+    @IBAction func showHide(sender: AnyObject) {
+        
+        viewBottomConstraint.constant = (viewBottomConstraint.constant == 0) ? -300 : 0
+        
+    }
+    
     
 }
 
